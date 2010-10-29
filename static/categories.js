59612fe35451d1f2bb8a8e488be096c07a398898
Jake Hickenlooper <jake@justcrazy.org>
2010-10-29 07:45:40 -0600

    changing the slider also changes max for others

--- static/categories.js
+++ static/categories.js
@@ -21,11 +21,38 @@ jQuery(document).ready(function($) {
       hash = {category:data};
       html = ich.expense_category_list(hash);
       $('#expense_category_list').html(html);
-      var available_balance = parseFloat($("#available-balance").text()).toFixed(2);
-      $("#expense_category_list div.balance-slider").slider({step:0.01}).each(function(){
-        var b = parseFloat($(this).text()).toFixed(2);
-        $(this).slider('option', 'value', b);
+      var available_balance = new Number(parseFloat($("#available-balance").text()));
+      $("#expense_category_list div.balance-slider").slider({
+        step: 0.01,
+        min: 0,
+        max: 0,
+        value: 0,
+        slide: function(event, ui) {
+          //$(this).siblings("span.balance").text(ui.value);
+        },
+        stop: function(event, ui) {
+          var start_balance = new Number(parseFloat($(this).siblings("span.balance").text()));
+          var new_balance = new Number(ui.value);
+          var change_balance = new_balance - start_balance;
+          //console.log("start = "+start_balance+" new = "+new_balance+" change = "+change_balance);
+          var this_max = ui.max;
+          $(this).addClass('current_slider');
+          $("div.balance-slider").not(".current_slider").each(function(e){ // TODO: filter out the current slider
+            var m = $(this).slider('option', 'max');
+            var v = $(this).slider('option', 'value');
+            var new_max = m-change_balance;
+            //console.log('change = '+change_balance+' max = '+m+' new max = '+new_max);
+            $(this).slider('option', 'max', new_max);
+            $(this).slider('option', 'value', v);
+          });
+          $(this).removeClass('current_slider');
+          $(this).siblings("span.balance").text(ui.value);
+        }
+      });
+      $("#expense_category_list div.balance-slider").each(function(){
+        var b = new Number(parseFloat($(this).siblings("span.balance").text()));
         $(this).slider('option', 'max', b+available_balance);
+        $(this).slider('option', 'value', b);
       });
     });
   };
@@ -39,7 +66,7 @@ jQuery(document).ready(function($) {
       };
 
     d = {'data_string':JSON.stringify(data_string)};
-    console.log(d.data_string);
+    //console.log(d.data_string);
     $.post("/"+db_name+"/expense", d, function(data){
       expense_category_list();
       total_balance();
